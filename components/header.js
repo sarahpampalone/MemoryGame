8703d74b51e1c795a0da0e1cfbfa998093a49a90
alexaper12 <130948432+alexaper12@users.noreply.github.com>
2023-04-24 16:11:17 -0400

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,3 +1,19 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
+export default function Header() {
+  return (
+    <View style = {styles.header}>
+      <Text style = {styles.header_text}>MemoryGame</Text>
+  </View>
+);
+}
+
+const styles = Style.Sheet.create({
+  header: {
+    flex: 1,
+    flexDirection: 'column',
+    alignSelf: 'stretch',
+    paddingTop: 20,
+    paddingBottom: 5,
+    
