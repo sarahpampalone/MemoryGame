ad0e125a685fc28749b0b4d9b5f6016f1db03aba
alexaper12 <130948432+alexaper12@users.noreply.github.com>
2023-04-24 16:33:20 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,34 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
+##import { Ionicons, FontAwesome, Entypo } from '@expo/vector-icons'; Kate
+import Header from './components/Header';
+import Score from './components/Score';
+import Card from './components/Card';
+import helpers from './components/helpers';
 
-export default function App() {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderCards = this.renderCards.bind(this);
+    this.resetCards = this.resetCards.bind(this);
+    let sources = {
+      'fontawesome': fontAwesome,
+      'entypo': Entypo,
+      'ionicons': Ionicons
+    };
+    let cards = [
+      src: 'fontawesome',
+      name: 'heart',
+      color: 'red',
+      },
+  {
+      src: 'entypo',
+      name: 'feather',
+      color: '#7db
+ 
+    
+    
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
