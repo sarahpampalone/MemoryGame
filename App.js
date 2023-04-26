1f5833a62a26d12b5c15de71f248f3f628536dff
alexaper12 <130948432+alexaper12@users.noreply.github.com>
2023-04-25 23:29:27 -0400

    Update App.js

--- App.js
+++ App.js
@@ -70,21 +70,142 @@ export default class App extends React.Component {
     ];
    }
   }
-    
-    
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   let clone = JSON.parse(JSON.stringify(cards));
+   this.cards = cards.concat(clone);
+   this.cards.map((obj) = > {
+   let id = Math.random().toString(36).substring(7);
+   obj.id = id;
+   obj.src = sources[obj.src];
+   obj.is_open = false;
+ });
+ this.cards= this.cards.shuffle();
+ this.state = {
+  current_selection: [],
+  selected_pairs: [].
+  score: 0,
+  cards: this.cards
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+render() {
+  return (
+    <View style {styles.container}>
+      <Header />
+      <View style = {styles.body}>
+        {
+          this.renderRows.call(this)
+         }
+       </View>
+       <Score score = {this.state.score} />
+       <Button 
+          onPress = {this.resetCards}
+          title = "Reset"
+          color = "#008CFA"
+        />
+      </View>
+     );
+   }
+   renderRows() {
+    let contents = this.getRowContents(this.state.cards);
+    return contents.map((cards, index) => {
+      return (
+        <View key={index} style={styles.row}>
+          { this.renderCards(cards) }
+        </View>
+      );
+     });
+    }
+    getRowContents(cards) {
+      let contents_r = [];
+      let contents = [];
+      let count = 0;
+      cards.forEach((item) => {
+        count += 1;
+        contents.push(item);
+        if(counts == 4){
+          contents_r.push(contents)
+          count = 0;
+          contents = [];
+        }
+     });
+     return contents_r;
+    }
+    renderCards(cards) {
+      return cards.map((card, index) => {
+        return (
+          <Card
+            key={index}
+            src={card.src}
+            name={card.name}
+            color={card.color}
+            is_open={card.is_open}
+            clickCard={this.clickCard.bind(this, card.id)}
+            />
+           );
+          });
+        }
+        clickCard(id) {
+          let selected_pairs = this.state.selected_pairs;
+          let current_selection = this.state.current_selection;
+          let score = this.state.score;
+          let index = this.state.cards.findIndex((card) => {
+            return card.id == id;
+          });
+          let cards = this.state.cards;
+          if(cards[index].is_open == false && selected_pairs.indexOf(cards[index].name === -1){
+            cards[index].is_open = true;
+              index: index,
+              name: cards[index].name
+            });
+         }
+         }
+         if(current_selection.length == 2){
+          if(current_selection[0].name == current_selection[1].name){
+            score += 1;
+            selected_pairs.push(cards[index].name);
+          }else{
+            cards[current_selection[0].index].is_open = false;
+            setTimeout(() => {
+              cards[index].is_open = false;
+              this.setState({
+                cards: cards
+               });
+               }, 500);
+              }
+              current_selection = [];
+             }
+             this.setState({
+              score: score,
+              cards: cards,
+              current_selection: 
+            });
+            resetCards() {
+              let cards = this.cards.map((obj) => {
+                obj.is_open = false;
+                return obj;
+              });
+              cards = cards.shuffle();
+              this.setState({
+                current_selection: [],
+                selected_pairs: [],
+                cards: cards,
+                score: 0
+              });
+             }
+             const styles = StyleSheet.create({
+              container: {
+                flex: 1,
+                alignSelf: 'stretch',
+                backgroundColor: '#fff'
+              },
+              row: {
+                flex: 1,
+                flexDirection: 'row'
+              },
+              body: {
+                flex: 18,
+                justifyContent: 'space-between',
+                padding: 10,
+                marginTop: 20
+               }
+              });
+                
+                
